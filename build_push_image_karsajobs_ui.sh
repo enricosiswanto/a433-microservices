d1d42ba724bf970285e522435fbcdb90d47dd79a
enricosiswanto <enrico@siswanto.me>
2025-04-23 15:50:56 +0800

    add build and CI script

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build image dari Dockerfile dengan tag sesuai format GitHub Packages
+docker build -t ghcr.io/$GHCR_USERNAME/karsajobs-ui:latest .
+
+# Login ke GitHub Packages
+echo $GHCR_PWD | docker login ghcr.io -u $GHCR_USERNAME --password-stdin
+
+# Push image ke GitHub Packages
+docker push ghcr.io/$GHCR_USERNAME/karsajobs-ui:latest
